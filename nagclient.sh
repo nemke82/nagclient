9558d3009bc29642391d524c2eaa0c45ca9ab320
Nemanja Djuric <ne@nemanja.io>
2016-10-06 17:57:44 +0200

    Add files via upload

--- nagclient.sh
+++ nagclient.sh
@@ -2,14 +2,14 @@
 #Author: Nemanja Djuric
 
 #Function defs
-setup_centos(){
-
-echo "Welcome to the Nagios Client installer. First, lets check if installation exist and remove it."
-if [ ! -d /etc/nagios ];then
-yum -y remove nagios-plugins-all nrpe lm_sensors ;
-else
+function setup_centos(){
 
 #Begin of Epel-repo install block. Version check and if exist.
+echo "Welcome to the Nagios Client installer. First, lets check if installation exist and remove it."
+yum -y remove nagios-plugins-all &&
+yum -y remove nrpe &&
+yum -y remove lm_sensors &&
+
 echo "Installing fresh installation"
 osversion=$(cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+'|cut -d"." -f1)
 echo ${osversion}
@@ -19,47 +19,45 @@ echo "Starting nrpe daemon and adding it to the chkconfig for autoboot functiona
 yum -y install http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum -y --enablerepo="epel" install nagios-plugins-all nrpe lm_sensors hddtemp &&
 service nrpe restart &&
-chkconfig nrpe on &&
-done
+chkconfig nrpe on
+
 elif [ ${osversion} -eq 7 ];then
 echo "Checking/Installing Epel repository CentOS 7, Nagios Plugins, Nrpe, Lmsensors and Hdd temperateure module."
 echo "Starting nrpe daemon and adding it to the chkconfig for autoboot functionality."
 yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 yum -y --enablerepo="epel" install nagios-plugins-all nrpe lm_sensors hddtemp &&
-systemctl restart nrpe
+systemctl restart nrpe &&
 systemctl enable nrpe
 echo "NOTE: On few CentOS 7 version's I've noticed that PID in nrpe.cfg needs to be changed from /var/run/nrpe.pid to /var/run/nrpe/nrpe.pid"
 echo "      Make sure to check systemctl status nrpe when this is installed to check if daemon is started properly."
-done
 else
 echo "ERROR: Unknown OS Version detected. Aborting Install "
 fi
 
 #Configuration block
 echo "NOTE: What type of server is this? KVM/XEN, OpenVZ or Dedicated?"
-echo "Please enter type as (cloud, vz or dedi):"
+echo "Please enter type as (1 - cloud, 2 - vz or 3 - dedi):"
 read stype
 
-if [ ${stype} -eq cloud ];then
+if [ ${stype} -eq 1 ];then
 rm -f /etc/nagios/nrpe.cfg &&
 echo "Getting nrpe.cfg file with installed commands.."
-wget -c http://repo.nemanja.io/nrpe.cfg /etc/nagios/nrpe.cfg &&
+wget -O /etc/nagios/nrpe.cfg http://repo.nemanja.io/nrpe.cfg
 echo "Complete!"
-done
-elif [ ${stype} -eq vz ];then
+fi
+if [ ${stype} -eq 2 ];then
 rm -f /etc/nagios/nrpe.cfg &&
 echo "Getting nrpe.cfg file with installed commands.."
-wget -c http://repo.nemanja.io/nrpe-vz.cfg /etc/nagios/nrpe.cfg &&
+wget -O /etc/nagios/nrpe.cfg http://repo.nemanja.io/nrpe-vz.cfg
 echo "Complete!"
-done
-if [ ${stype} -eq dedi ];then
+fi
+if [ ${stype} -eq 3 ];then
 rm -f /etc/nagios/nrpe.cfg &&
 echo "Getting nrpe.cfg file with installed commands.."
-wget -c http://repo.nemanja.io/nrpe-dedi.cfg /etc/nagios/nrpe.cfg &&
+wget -O /etc/nagios/nrpe.cfg http://repo.nemanja.io/nrpe-dedi.cfg &&
 echo "Complete!"
-done
+fi
 
-else
 echo "Adding Nagios username to /etc/sudoers file with limited access only to /usr/lib64/nagios/plugins/ folder."
 sed -i '$ a nagios ALL=(ALL) NOPASSWD: /usr/lib64/nagios/plugins/' /etc/sudoers &&
 echo "Complete!"
@@ -68,34 +66,57 @@ echo "Removing requiretty from /etc/sudoers.. Sudo and Nagios cannot work with t
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 echo "Complete!"
 
-service nrpe stop || systemctl stop nrpe &&
-service nrpe start || systemctl start nrpe &&
-		done
-	}
+echo "Removing current plugins installed in /usr/lib64/nagios/plugins folder and installing ours."
+rm -f /usr/lib64/nagios/plugins/* &&
+wget -O /etc/nagios/plugins.tar http://repo.nemanja.io/plugins.tar &&
+tar -xvf /etc/nagios/plugins.tar -C /usr/lib64/nagios/plugins
+echo "Complete!"
+
+echo "Restarting Nrpe Daemon..."
+service nrpe stop || systemctl stop nrpe
+service nrpe start || systemctl start nrpe
+echo "Mission completed! You can review what commands are enabled in /etc/nagios/nrpe.cfg file."
+echo "Thank you for using Nagios Client installer for CentOS/RHEL."
+
+}
 
 setup_ubuntu(){
 echo "Welcome to the Nagios Client installer (for Debian/Ubuntu). First, lets check if installation exist and remove it."
 if [ ! -d /etc/nagios ];then
-apt-get -y remove nagios-nrpe-server nagios-plugins lm-sensors ;
+apt-get -y remove nagios-nrpe-server &&
+apt-get -y remove nagios-plugins &&
+apt-get -y remove lm-sensors
 else
 echo "Checking/Installing Nagios Plugins, Nrpe, Lmsensors and Hdd temperateure module."
 echo "Starting nrpe daemon."
 apt-get -y install nagios-nrpe-server nagios-plugins lm-sensors unzip vim hddtemp &&
 echo "Starting nrpe daemon."
-done
 
 fi
 
 #Configuration block
 echo "NOTE: What type of server is this? KVM/XEN, OpenVZ or Dedicated?"
-echo "Please enter type as (cloud, vz or dedi):"
+echo "Please enter type as (1 - cloud, 2 - vz or 3 - dedi):"
 read stype
 
-if [ ${stype} -eq cloud ];then
+if [ ${stype} -eq 1 ];then
 rm -f /etc/nagios/nrpe.cfg &&
 echo "Getting nrpe.cfg file with installed commands.."
-wget -c http://repo.nemanja.io/nrpe.cfg /etc/nagios/nrpe.cfg &&
+wget -O /etc/nagios/nrpe.cfg http://repo.nemanja.io/nrpe.cfg
 echo "Complete!"
+fi
+if [ ${stype} -eq 2 ];then
+rm -f /etc/nagios/nrpe.cfg &&
+echo "Getting nrpe.cfg file with installed commands.."
+wget -O /etc/nagios/nrpe.cfg http://repo.nemanja.io/nrpe-vz.cfg
+echo "Complete!"
+fi
+if [ ${stype} -eq 3 ];then
+rm -f /etc/nagios/nrpe.cfg &&
+echo "Getting nrpe.cfg file with installed commands.."
+wget -O /etc/nagios/nrpe.cfg http://repo.nemanja.io/nrpe-dedi.cfg &&
+echo "Complete!"
+fi
 
 echo "Adding Nagios username to /etc/sudoers file with limited access only to /usr/lib64/nagios/plugins/ folder."
 sed -i '$ a nagios ALL=(ALL) NOPASSWD: /usr/lib64/nagios/plugins/' /etc/sudoers &&
@@ -105,23 +126,39 @@ echo "Removing requiretty from /etc/sudoers.. Sudo and Nagios cannot work with t
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 echo "Complete!"
 
-echo "Restarting nrpe daemon"
+echo "Removing current plugins installed in /usr/lib64/nagios/plugins folder and installing ours."
+rm -f /usr/lib64/nagios/plugins/* &&
+wget -O /etc/nagios/plugins.tar http://repo.nemanja.io/plugins.tar &&
+tar -xvf /etc/nagios/plugins.tar -C /usr/lib64/nagios/plugins
+echo "Complete!"
+
+echo "Restarting Nrpe Daemon..."
 /etc/init.d/nagios-nrpe-server restart
 echo "Complete!"
 
 echo "Adding port 5666 to firewall"
 ufw allow 5666/tcp
 echo "Complete!"
-		done
-	}
+
+echo "Mission completed! You can review what commands are enabled in /etc/nagios/nrpe.cfg file."
+echo "Thank you for using Nagios Client installer for CentOS/RHEL."
+
+}
 
 auto_setup(){
+read -p "Are you sure you want to continue? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
 if [ -f /etc/redhat-release ]; then
 setup_centos
 fi
 
 if [ -f /etc/lsb-release ]; then
 setup_ubuntu
+fi
+
+else
+  exit 0
 fi
 }
 
@@ -142,3 +179,4 @@ if [ ! -z $1 ];then
 else
 	auto_setup
 fi
+
